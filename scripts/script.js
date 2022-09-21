b3f220784bad5c502a08647bd894702d585e3559
Fayaaz036 <fayaaz.nabie@hva.nl>
2022-09-22 00:49:55 +0200

    page 1 finished (JS Added)

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
-console.log("hi");
+var button = document.querySelector("nav > button")
+var nav = document.querySelector("header>nav")
+
+
+button.addEventListener('click', toggleMenu);
+
+
+function toggleMenu(){
+    nav.classList.toggle("showmenu")
+}
+
+window.onload = function(){
+    document.getElementById('videoplayer').play()
+}
