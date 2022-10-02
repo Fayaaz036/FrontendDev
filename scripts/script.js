6508acbd92aad274a7baea1aec290f6f2ea73ec0
Fayaaz036 <fayaaz.nabie@hva.nl>
2022-10-02 19:56:05 +0200

    Final version. Klaar voor eindgesprek en alles doorlopen zondag 9 opt. 19:52

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 // JavaScript Document
-const button = document.querySelector("nav > button");
+const button = document.querySelector("nav  > button");
 const nav = document.querySelector("header > nav");
-const loading = document.getElementsByClassName("loading");''
 
 
 button.addEventListener('click', toggleMenu);
